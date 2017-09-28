6bcc7c218334709526223bbf3ca7ad6045da939f
Alex5801 <1134698440@qq.com>
2017-09-28 15:22:12 +0800

    first

---
+++ newworld/views.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+       #return HttpResponse(u"Hello World! 大家好!")
+       return render(request,"index.html", )
+
